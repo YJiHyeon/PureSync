2555ffc7a04bd5e5bcf17049d6541893b2c0a043
Ji Hyeon <127700904+YJiHyeon@users.noreply.github.com>
2023-11-22 10:46:20 +0900

    Initial commit

---
+++ src/main/java/com/fcc/PureSync/PureSyncApplication.java
@@ -0,0 +1,13 @@
+package com.fcc.PureSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureSyncApplication.class, args);
+	}
+
+}
