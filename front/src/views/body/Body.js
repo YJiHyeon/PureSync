0e5e04acf328b900879d94d47478602dcf07603b
Ji Hyeon <127700904+YJiHyeon@users.noreply.github.com>
2023-11-29 11:03:34 +0900

    [feat] 요약 박스 완성

--- front/src/views/body/Body.js
+++ front/src/views/body/Body.js
@@ -3,6 +3,7 @@ import { DatePicker, Button } from 'components/ui'
 import Menu from 'components/body/menu'
 import Exercise from 'components/body/exercise'
 import Axios from 'axios'
+import Summry from 'components/body/summary'
 
 const BodyMenu = () => {
 
@@ -12,13 +13,13 @@ const BodyMenu = () => {
     const [selectDate, setSelectDate] = useState(today);
     const [menuList, setMenuList] = useState([]);
     const [exerciseList, setExerciseList] = useState([]);
+    const [summaryList, setSummaryList] = useState([]);
 
 
     const dataLoad = () => {
 
         Axios.get(`http://127.0.0.1:9000/api/menu/list?mem_seq=1&menu_date=${selectDate}`)
             .then((res) => {
-                console.log(res.data)
             })
             .catch((res) => {
                 console.log(res);
@@ -33,11 +34,10 @@ const BodyMenu = () => {
     useEffect(() => {
         console.log("menu에서 useEffect()호출");
         dataLoad();
-    }, [menuList], [exerciseList] );
+    }, [menuList], [exerciseList], [summaryList] );
 
     return (
         <>
-            <div>BodyMenu</div>
             <DatePicker placeholder="날짜를 선택하세요" DatePickerClick={DatePickerClick} />
             <br />
             <Menu selectDate={selectDate} />
@@ -45,6 +45,10 @@ const BodyMenu = () => {
             <hr />
             <br />
             <Exercise selectDate={selectDate} />
+            <br />
+            <hr />
+            <br />
+            <Summry selectDate={selectDate}/>
         </>
     )
 }
