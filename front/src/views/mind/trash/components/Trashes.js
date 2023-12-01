f35761b51de2f01617d8107b8d4100c2a44a3122
Hayoung Yu <nhy8001@gmail.com>
2023-12-01 10:42:07 +0900

    [Design] 수정 trash, diary
    - trashList 디자인 수정
    - diaryList 디자인 수정

--- front/src/views/mind/trash/components/Trashes.js
+++ front/src/views/mind/trash/components/Trashes.js
@@ -26,7 +26,7 @@ const Trashes = (props) => {
     };
 
     return (
-        <div className="mb-6 grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-4 gap-4">
             {trashes.map((trash, index) => (
                 <Card bordered key={trash.tsSeq}>
                     <div className="min-h-[60px]">
