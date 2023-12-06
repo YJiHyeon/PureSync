e494541caa6956e9f92f0d8f2e162e044a16eacf
Ji Hyeon <127700904+YJiHyeon@users.noreply.github.com>
2023-12-06 13:05:48 +0900

    [docs] 어드민 보드 경로 수정

---
+++ src/main/java/com/fcc/PureSync/service/AdminBoardService.java
@@ -0,0 +1,16 @@
+package com.fcc.PureSync.service;
+import com.fcc.PureSync.dao.AdminBoardDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminBoardService {
+
+    private final AdminBoardDao adminBoardDao;
+
+
+
+
+
+}
