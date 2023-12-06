8183960b35f793d7772d28ee6a71baeeec46b81f
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-06 23:05:11 +0900

    feat: TestAnswerRepository 수정 - 기존 메소드 삭제 (사용안함)

--- src/main/java/com/fcc/PureSync/repository/TestAnswerRepository.java
+++ src/main/java/com/fcc/PureSync/repository/TestAnswerRepository.java
@@ -7,8 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TestAnswerRepository extends JpaRepository<TestAnswer,Long> {
-
-    int countByTestQuestion_QueSeqAndMember_MemSeq(Long queSeq, Long memSeq);
-
     List<TestAnswer> findByMember_MemSeqAndTestSeq(Long memSeq, Long testSeq, Pageable pageable);
 }
