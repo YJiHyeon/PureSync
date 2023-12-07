2eea633bf30e5e00636be78808a477d2eaadf496
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-07 17:42:04 +0900

    feat: TestAnswerRepository 수정 - ansInfo 를 사용하는 메소드로 수정

--- src/main/java/com/fcc/PureSync/repository/TestAnswerRepository.java
+++ src/main/java/com/fcc/PureSync/repository/TestAnswerRepository.java
@@ -1,11 +1,9 @@
 package com.fcc.PureSync.repository;
 
+import com.fcc.PureSync.entity.Member;
 import com.fcc.PureSync.entity.TestAnswer;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TestAnswerRepository extends JpaRepository<TestAnswer,Long> {
-    List<TestAnswer> findByMember_MemSeqAndTestSeq(Long memSeq, Long testSeq, Pageable pageable);
+    TestAnswer findByMemberAndAnsInfo(Member member, Integer ansInfo);
 }
