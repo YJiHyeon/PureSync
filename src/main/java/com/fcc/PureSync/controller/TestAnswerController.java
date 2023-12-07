a353c54cf11c2ee2b943e501c9fba82539cebd45
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-07 17:44:25 +0900

    feat: TestAnswerService, TestAnswerController 수정 - ansInfo 필드 추가 - depression 기능 추가 - 전체 조회 -> 단일 조회로 변경 - 수정 기능 추가

--- src/main/java/com/fcc/PureSync/controller/TestAnswerController.java
+++ src/main/java/com/fcc/PureSync/controller/TestAnswerController.java
@@ -4,7 +4,6 @@ import com.fcc.PureSync.dto.ResultDto;
 import com.fcc.PureSync.dto.TestAnswerDto;
 import com.fcc.PureSync.service.TestAnswerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,13 +18,28 @@ public class TestAnswerController {
         return testAnswerService.stressAnswer(testAnswerDto, id);
     }
 
-    @GetMapping("/stress/answer")
-    public ResultDto getAllStressAnswer(Pageable pageable,String id){
-        return testAnswerService.getAllStressAnswer(pageable,id);
+    @PostMapping("/depression")
+    public ResultDto depressionAnswer(@RequestBody TestAnswerDto testAnswerDto, String id) {
+        return testAnswerService.depressionAnswer(testAnswerDto, id);
     }
 
-    @GetMapping("/depression/answer")
-    public ResultDto getAllDepressionAnswer(Pageable pageable,String id){
-        return testAnswerService.getAllDepressionAnswer(pageable,id);
+    @GetMapping("/stress/answer/{memSeq}/{ansInfo}")
+    public ResultDto getAllStressAnswer(@PathVariable Long memSeq, @PathVariable Integer ansInfo){
+        return testAnswerService.getAllStressAnswer(memSeq, ansInfo);
+    }
+
+    @GetMapping("/depression/answer/{memSeq}/{ansInfo}")
+    public ResultDto getAllDepressionAnswer(@PathVariable Long memSeq, @PathVariable Integer ansInfo){
+        return testAnswerService.getAllDepressionAnswer(memSeq, ansInfo);
+    }
+
+    @PutMapping("/stress/{memSeq}/{ansInfo}")
+    public ResultDto updateStressAnswer(@RequestBody TestAnswerDto testAnswerDto, @PathVariable Long memSeq, @PathVariable Integer ansInfo) {
+        return testAnswerService.updateStressAnswer(testAnswerDto, memSeq, ansInfo);
+    }
+
+    @PutMapping("/depression/{memSeq}/{ansInfo}")
+    public ResultDto updateDepressionAnswer(@RequestBody TestAnswerDto testAnswerDto, @PathVariable Long memSeq, @PathVariable Integer ansInfo) {
+        return testAnswerService.updateDepressionAnswer(testAnswerDto, memSeq, ansInfo);
     }
 }
--- src/main/java/com/fcc/PureSync/service/TestAnswerService.java
+++ src/main/java/com/fcc/PureSync/service/TestAnswerService.java
@@ -9,12 +9,10 @@ import com.fcc.PureSync.exception.CustomExceptionCode;
 import com.fcc.PureSync.repository.MemberRepository;
 import com.fcc.PureSync.repository.TestAnswerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.List;
 
 import static com.fcc.PureSync.dto.TestAnswerDto.toDto;
 
@@ -46,6 +44,7 @@ public class TestAnswerService {
         } else {
             TestAnswer testAnswer = TestAnswer.builder()
                     .testSeq(testSeq)
+                    .ansInfo(1)
                     .testAns(testAnswerDto.getTestAns())
                     .member(member)
                     .build();
@@ -58,33 +57,96 @@ public class TestAnswerService {
         }
     }
 
-    public ResultDto getAllStressAnswer(Pageable pageable, String id) {
-        Long testSeq = 1L;
-        id = "aaa";
-        Member member = memberRepository.findByMemId(id)
-                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
-        List<TestAnswer> allStressAnwswer = testAnswerRepository.findByMember_MemSeqAndTestSeq(member.getMemSeq(),testSeq,pageable);
-        List<TestAnswerDto> allStressAnwswerDto = allStressAnwswer.stream()
-                .map(TestAnswerDto::toDto)
-                .toList();
-
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("allStressAnwswer", allStressAnwswerDto);
-        return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "스트레스 테스트 답변 전체 조회 성공", map);
-    }
-
-    public ResultDto getAllDepressionAnswer(Pageable pageable, String id) {
+    public ResultDto depressionAnswer(TestAnswerDto testAnswerDto, String id) {
         Long testSeq = 2L;
         id = "aaa";
+
         Member member = memberRepository.findByMemId(id)
                 .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
-        List<TestAnswer> allDepressionAnwswer = testAnswerRepository.findByMember_MemSeqAndTestSeq(member.getMemSeq(),testSeq,pageable);
-        List<TestAnswerDto> allDepressionAnwswerDto = allDepressionAnwswer.stream()
-                .map(TestAnswerDto::toDto)
-                .toList();
+
+        if (testSeq != 2L) {
+            throw new CustomException(CustomExceptionCode.NOT_FOUND_TEST);
+        } else {
+            TestAnswer testAnswer = TestAnswer.builder()
+                    .testSeq(testSeq)
+                    .ansInfo(2)
+                    .testAns(testAnswerDto.getTestAns())
+                    .member(member)
+                    .build();
+
+            testAnswerRepository.save(testAnswer);
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("depressionTestAnswer", toDto(testAnswer));
+
+            return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "우울증 테스트 답변 완료", map);
+        }
+    }
+
+    public ResultDto getAllStressAnswer(Long memSeq, Integer ansInfo) {
+        Member member = memberRepository.findById(memSeq)
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
+
+        TestAnswer testAnswer = testAnswerRepository.findByMemberAndAnsInfo(member, ansInfo);
 
         HashMap<String, Object> map = new HashMap<>();
-        map.put("allDepressionAnwswer", allDepressionAnwswerDto);
-        return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "우울증 테스트 답변 전체 조회 성공", map);
+        map.put("allStressAnswer", testAnswer);
+        return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "스트레스 테스트 답변 조회 성공", map);
+    }
+
+    public ResultDto getAllDepressionAnswer(Long memSeq, Integer ansInfo) {
+        Member member = memberRepository.findById(memSeq)
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
+
+        TestAnswer testAnswer = testAnswerRepository.findByMemberAndAnsInfo(member, ansInfo);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("allDepressionAnswer", testAnswer);
+        return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "우울증 테스트 답변 조회 성공", map);
+    }
+
+    public ResultDto updateStressAnswer(TestAnswerDto testAnswerDto, Long memSeq, Integer ansInfo) {
+        Member member = memberRepository.findById(memSeq)
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
+
+        // 기존 데이터를 찾기
+        TestAnswer testAnswer = testAnswerRepository.findByMemberAndAnsInfo(member, ansInfo);
+
+        // 기존 데이터 업데이트
+        TestAnswer updatedTestAnswer = TestAnswer.builder()
+                .ansSeq(testAnswer.getAnsSeq())
+                .testSeq(testAnswer.getTestSeq())
+                .ansInfo(1)
+                .testAns(testAnswerDto.getTestAns())
+                .member(member)
+                .build();
+
+        testAnswerRepository.save(updatedTestAnswer);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("stressTestAnswer", toDto(updatedTestAnswer));
+
+        return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "스트레스 테스트 답변 수정 완료", map);
+    }
+
+    public ResultDto updateDepressionAnswer(TestAnswerDto testAnswerDto, Long memSeq, Integer ansInfo) {
+        Member member = memberRepository.findById(memSeq)
+                .orElseThrow(() -> new CustomException(CustomExceptionCode.NOT_FOUND_USER));
+
+        TestAnswer testAnswer = testAnswerRepository.findByMemberAndAnsInfo(member, ansInfo);
+
+        TestAnswer updatedTestAnswer = TestAnswer.builder()
+                .ansSeq(testAnswer.getAnsSeq())
+                .testSeq(testAnswer.getTestSeq())
+                .ansInfo(2)
+                .testAns(testAnswerDto.getTestAns())
+                .member(member)
+                .build();
+
+        testAnswerRepository.save(updatedTestAnswer);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("depressionTestAnswer", toDto(updatedTestAnswer));
+
+        return buildResultDto(HttpStatus.OK.value(), HttpStatus.OK, "우울증 테스트 답변 수정 완료", map);
     }
 }
