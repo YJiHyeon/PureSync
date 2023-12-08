64790fbd7694ba0e430518c98078e074ae2c725c
Hyunbo <80660768+simhyunbo@users.noreply.github.com>
2023-12-08 17:51:27 +0900

    [fix] theme
    theme

--- front/src/views/auth/SignIn/index.js
+++ front/src/views/auth/SignIn/index.js
@@ -5,7 +5,8 @@ const SignIn = () => {
     return (
         <>
             <div className="mb-8">
-                <h3 className="mb-1">Welcome!</h3>
+                <h3 className="mb-1">Welcome back!</h3>
+                <p>Please enter your credentials to sign in!</p>
             </div>
             <SignInForm disableSubmit={false} />
         </>
