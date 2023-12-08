5036b2bed68c99bb0b6c25a617c65eda1bd463da
Hayoung Yu <nhy8001@gmail.com>
2023-12-08 12:48:13 +0900

    [Design] 정리 admin main
    - admin main 디자인 정리

--- src/main/java/com/fcc/PureSync/repository/VisitorRepository.java
+++ src/main/java/com/fcc/PureSync/repository/VisitorRepository.java
@@ -1,13 +1,8 @@
 package com.fcc.PureSync.repository;
 
-import com.fcc.PureSync.dto.ResultDto;
 import com.fcc.PureSync.entity.Visitor;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 
-import java.util.HashMap;
 
 public interface VisitorRepository extends JpaRepository <Visitor, Long> {
     Visitor findByVisitorDateAndVisitorIp(String visitorDate, String visitorIp);
