cee9f502a6cbc6ab193fe9c52fef8ddfb770a549
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-16 16:45:16 +0900

    fix: Editor.js 수정 - token 적용 로직 추가

--- front/src/views/qnaBoard/EditArticle/components/Editor.js
+++ front/src/views/qnaBoard/EditArticle/components/Editor.js
@@ -18,6 +18,9 @@ import * as Yup from 'yup'
 import axios from 'axios'
 import Upload from 'components/ui/Upload'
 import { useLocation } from 'react-router-dom';
+import getHeaderCookie from 'utils/hooks/getHeaderCookie'
+import { parseJwt, getMemInfoFromToken } from 'utils/hooks/parseToken'
+
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 // const validationSchema = Yup.object().shape({
 //     title: Yup.string().required('Title required'),
@@ -27,8 +30,10 @@ axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 
 
 const Editor = () => {
+  const access_token = getHeaderCookie();
+  let parse_token = parseJwt(access_token);
+  let { memId, memSeq } = getMemInfoFromToken(parse_token);
   const navigate = useNavigate();
-  const memId = 'aaa';
   const { state } = useLocation();
   const { updateData } = state || {};
   console.log(updateData);
@@ -76,28 +81,25 @@ const Editor = () => {
         const response = await axios.post('http://localhost:9000/api/qnaBoard', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
-
+            Authorization: `Bearer ${access_token}`
           },
-          data: formData,
         });
 
         console.log('파일 업로드 성공:', response.data);
-        alert('게시글이 작성되었습니다.');
+        // alert('게시글이 작성되었습니다.');
         navigate('/qnaBoard');
       } else {
         console.log(updateData);
 
-        const response = await axios.put(`http://localhost:9000/api/qnaBoard/${updateData.articleId}`, formData, {
+        const response = await axios.put(`http://localhost:9000/api/qnaBoard/${updateData.articleId}`, formData,  {
           headers: {
             'Content-Type': 'multipart/form-data',
-
+            Authorization: `Bearer ${access_token}`
           },
-          data: formData,
         });
 
         console.log('파일 업로드 성공:', response.data);
-
-        alert('게시글이 수정되었습니다.');
+        // alert('게시글이 수정되었습니다.');
         navigate('/qnaBoard');
       }
     } catch (error) {
@@ -113,7 +115,7 @@ const Editor = () => {
       initialValues={{
         qnaBoardName: updateData ? updateData.qnaBoardName : '',
         qnaBoardContents: updateData ? updateData.qnaBoardContents : '',
-        memId: memId,
+        memId: '',
         files: updateData ? updateData.qnaBoardFile : '',
       }}
       onSubmit={(values, { setSubmitting }) => {
@@ -122,6 +124,7 @@ const Editor = () => {
     >
       {({ values, touched, errors, isSubmitting, setFieldValue }) => (
         <Form enctype="multipart/form-data" name="myform">
+          <input type='hidden' value={memId} name="memId" />
           <FormContainer>
             <FormItem label="제목">
               <Field autoComplete="off" name="qnaBoardName" component={Input} />
@@ -147,7 +150,7 @@ const Editor = () => {
                 fileList={field.value}
               />
             }} */}
-            
+
             {/* <div className="upload-file-list">
               {files.map((file, index) => (
                 <FileItem file={file} key={file.qnaBoardfileName + index}>
